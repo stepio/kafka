9ebe1d85d2cc772eb333cb9b7369749fb123c99b
Igor Stepanov <stepio.ua@gmail.com>
2017-01-02 00:26:28 +0200

    KAFKA-3923: Use 'assertj' project for implementing unit tests

--- clients/src/test/java/org/apache/kafka/common/metrics/JmxReporterTest.java
+++ clients/src/test/java/org/apache/kafka/common/metrics/JmxReporterTest.java
@@ -24,100 +24,116 @@ import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.metrics.stats.Avg;
 import org.apache.kafka.common.metrics.stats.Total;
 import org.apache.kafka.common.utils.Time;
-import org.hamcrest.CoreMatchers;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class JmxReporterTest {
 
     @Test
-    public void testJmxRegistration() throws Exception {
+    public void testJmxRegistration() {
         final JmxReporter reporter = new JmxReporter();
         MetricName name;
         try (Metrics metrics = new Metrics()) {
-            assertEquals(0, reporter.size());
+            assertThat(reporter.size())
+                    .withFailMessage("List of 'mbeans' in 'JmxReporter' should be empty").isZero();
             metrics.addReporter(reporter);
-            assertEquals(1, reporter.size());
+            assertThat(reporter.size())
+                    .withFailMessage("Size of 'mbeans' in 'JmxReporter' should be incremented by 1").isEqualTo(1);
             Sensor sensor = metrics.sensor("kafka.requests");
             name = metrics.metricName("pack.bean1.avg", "grp1");
             sensor.add(name, new Avg());
-            assertEquals(2, reporter.size());
-            assertEquals(1, reporter.getMBean(name).size());
+            assertThat(reporter.size())
+                    .withFailMessage("Size of 'mbeans' in 'JmxReporter' should be incremented by 1").isEqualTo(2);
+            assertThat(reporter.getMBean(name).size())
+                    .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be incremented by 1").isEqualTo(1);
             name = metrics.metricName("pack.bean2.total", "grp2");
             sensor.add(name, new Total());
-            assertEquals(3, reporter.size());
-            assertEquals(1, reporter.getMBean(name).size());
+            assertThat(reporter.size())
+                    .withFailMessage("Size of 'mbeans' in 'JmxReporter' should be incremented by 1").isEqualTo(3);
+            assertThat(reporter.getMBean(name).size())
+                    .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be incremented by 1").isEqualTo(1);
             Sensor sensor2 = metrics.sensor("kafka.blah");
             name = metrics.metricName("pack.bean1.some", "grp1");
             sensor2.add(name, new Total());
-            assertEquals(3, reporter.size());
-            assertEquals(2, reporter.getMBean(name).size());
+            assertThat(reporter.size())
+                    .withFailMessage("Size of 'mbeans' in 'JmxReporter' should not change").isEqualTo(3);
+            assertThat(reporter.getMBean(name).size())
+                    .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be incremented by 1").isEqualTo(2);
             name = metrics.metricName("pack.bean2.some", "grp1");
             sensor2.add(name, new Total());
-            assertEquals(3, reporter.size());
-            assertEquals(3, reporter.getMBean(name).size());
+            assertThat(reporter.size())
+                    .withFailMessage("Size of 'mbeans' in 'JmxReporter' should not change").isEqualTo(3);
+            assertThat(reporter.getMBean(name).size())
+                    .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be incremented by 1").isEqualTo(3);
             metrics.removeSensor(sensor2.name());
-            assertEquals(3, reporter.size());
-            assertEquals(1, reporter.getMBean(name).size());
+            assertThat(reporter.size())
+                    .withFailMessage("Size of 'mbeans' in 'JmxReporter' should not change").isEqualTo(3);
+            assertThat(reporter.getMBean(name).size())
+                    .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be decremented by 2").isEqualTo(1);
         }
-        assertEquals(0, reporter.size());
-        assertEquals(0, reporter.getMBean(name).size());
+        assertThat(reporter.size())
+                .withFailMessage("List of 'mbeans' in 'JmxReporter' should be empty").isZero();
+        assertThat(reporter.getMBean(name).size())
+                .withFailMessage("List of 'metrics' in 'KafkaMbean' should be empty").isZero();
     }
 
     @Test
-    public void testJmxRegistrationWithMock() throws Exception {
+    public void testJmxRegistrationWithMock() {
         final JmxReporter reporter = new JmxReporter();
         MockKafkaMetric metric;
-        assertEquals(0, reporter.size());
+        assertThat(reporter.size())
+                .withFailMessage("List of 'mbeans' in 'JmxReporter' should be empty").isZero();
         metric = MockKafkaMetric.of("pack.bean1.avg", "grp1", new Avg());
         reporter.metricChange(metric);
-        assertEquals(1, reporter.size());
-        assertEquals(1, reporter.getMBean(metric.metricName()).size());
+        assertThat(reporter.size())
+                .withFailMessage("Size of 'mbeans' in 'JmxReporter' should be incremented by 1").isEqualTo(1);
+        assertThat(reporter.getMBean(metric.metricName()).size())
+                .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be incremented by 1").isEqualTo(1);
         MockKafkaMetric metricAnotherGroup = MockKafkaMetric.of("pack.bean2.total", "grp2", new Total());
         reporter.metricChange(metricAnotherGroup);
-        assertEquals(2, reporter.size());
-        assertEquals(1, reporter.getMBean(metricAnotherGroup.metricName()).size());
+        assertThat(reporter.size())
+                .withFailMessage("Size of 'mbeans' in 'JmxReporter' should be incremented by 1").isEqualTo(2);
+        assertThat(reporter.getMBean(metricAnotherGroup.metricName()).size())
+                .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be incremented by 1").isEqualTo(1);
         List<KafkaMetric> list = new ArrayList<KafkaMetric>();
         metric = MockKafkaMetric.of("pack.bean1.some", "grp1", new Total());
         list.add(metric);
         metric = MockKafkaMetric.of("pack.bean2.some", "grp1", new Total());
         list.add(metric);
         reporter.init(list);
-        assertEquals(2, reporter.size());
-        assertEquals(3, reporter.getMBean(metric.metricName()).size());
+        assertThat(reporter.size())
+                .withFailMessage("Size of 'mbeans' in 'JmxReporter' should not change").isEqualTo(2);
+        assertThat(reporter.getMBean(metric.metricName()).size())
+                .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be incremented by 2").isEqualTo(3);
         reporter.metricRemoval(metric);
-        assertEquals(2, reporter.size());
-        assertEquals(2, reporter.getMBean(metric.metricName()).size());
+        assertThat(reporter.size())
+                .withFailMessage("Size of 'mbeans' in 'JmxReporter' should not change").isEqualTo(2);
+        assertThat(reporter.getMBean(metric.metricName()).size())
+                .withFailMessage("Size of 'metrics' in 'KafkaMbean' should be decremented by 1").isEqualTo(2);
         reporter.metricRemoval(metricAnotherGroup);
-        assertEquals(1, reporter.size());
-        assertEquals(0, reporter.getMBean(metricAnotherGroup.metricName()).size());
+        assertThat(reporter.size())
+                .withFailMessage("Size of 'mbeans' in 'JmxReporter' should be decremented by 1").isEqualTo(1);
+        assertThat(reporter.getMBean(metricAnotherGroup.metricName()).size())
+                .withFailMessage("List of 'metrics' in 'KafkaMbean' should be empty").isZero();
         reporter.close();
-        assertEquals(0, reporter.size());
-        assertEquals(0, reporter.getMBean(metric.metricName()).size());
+        assertThat(reporter.size())
+                .withFailMessage("List of 'mbeans' in 'JmxReporter' should be empty").isZero();
+        assertThat(reporter.getMBean(metric.metricName()).size())
+                .withFailMessage("List of 'metrics' in 'KafkaMbean' should be empty").isZero();
     }
 
     @Test
     public void testGetMBeanNameWithNull() {
         final JmxReporter reporter = new JmxReporter();
-        try {
-            reporter.getMBeanName(null);
-        } catch (RuntimeException e) {
-            assertThat(e, CoreMatchers.instanceOf(NullPointerException.class));
-            assertThat(e.getMessage(), CoreMatchers.is("Parameter 'metricName' is mandatory"));
-        }
-    }
-
-    @Test
-    public void testGetMBeanWithNull() {
-        final JmxReporter reporter = new JmxReporter();
-        try {
-            reporter.getMBean(null);
-        } catch (RuntimeException e) {
-            assertThat(e, CoreMatchers.instanceOf(NullPointerException.class));
-            assertThat(e.getMessage(), CoreMatchers.is("Parameter 'metricName' is mandatory"));
-        }
+        assertThatThrownBy(new ThrowableAssert.ThrowingCallable() {
+            @Override
+            public void call() throws Throwable {
+                reporter.getMBeanName(null);
+            }
+        }).isInstanceOf(NullPointerException.class).hasMessage("Parameter 'metricName' is mandatory");
     }
 
     private static class MockKafkaMetric extends KafkaMetric {
